c64eba7c699f58ca4aa541901f6078f9e08186ac
Didi518 <kevin.ouali@gmail.com>
2024-04-30 07:50:54 +0200

    :memo: render messages

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,23 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MessageController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified'])->group(function() {
     Route::get('/', [HomeController::class, 'home'])->name('dashboard');
 
-    Route::get('/utilisateur/{user}', function() {
+    Route::get('/utilisateur/{user}', [MessageController::class, 'byUser'])->name('chat.user');
+    Route::get('/groupe/{group}', [MessageController::class, 'byGroup'])->name('chat.group');
 
-    })->name('chat.user');
-    Route::get('/groupe/{group}', function() {
-
-    })->name('chat.group');
+    Route::post('/message', [MessageController::class, 'store'])->name('message.store');
+    Route::delete('/message/{message}', [MessageController::class, 'destroy'])->name('message.destroy');
+    Route::get('/message/anciens/{message}', [MessageController::class, 'loadOlder'])->name('message.loadOlder');
 });
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profil', [ProfileController::class, 'edit'])->name('profil.edit');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profil.edit');
     Route::patch('/profil', [ProfileController::class, 'update'])->name('profil.update');
     Route::delete('/profil', [ProfileController::class, 'destroy'])->name('profil.destroy');
 });
